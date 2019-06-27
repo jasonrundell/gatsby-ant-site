59880279bbce1d20449f5c222f07061b23f11a42
Jason Rundell <jason.rundell@gmail.com>
2019-06-27 16:59:28 -0400

    Layout refactored to functional component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,22 @@
 import React from 'react'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import styles from './layout.module.scss'
 
-class Layout extends React.Component {
-  render() {
-    const { title, children } = this.props
-    return (
-      <>
-        <Header siteTitle={title} />
-        <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-        </div>
-      </>
-    )
-  }
-}
+const Layout = ({ title, children }) => (
+  <>
+    <Header siteTitle={title} />
+    <div className={styles.container}>
+    <main>{children}</main>
+    <footer>
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+    </div>
+  </>
+)
 
 Layout.propTypes = {
   title: PropTypes.string.isRequired,
