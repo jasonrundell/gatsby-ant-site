947b08fe6204febdb911a790a065ec8139129c11
Jason Rundell <jason.rundell@gmail.com>
2019-06-26 23:05:39 -0400

    Refactored layout, seo, and index
    Index now a React.Component for more performant child components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,17 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    return (
+      <>
+        <Header siteTitle={title} />
+        <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
@@ -40,12 +25,14 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
-    </>
-  )
+        </div>
+      </>
+    )
+  }
 }
 
 Layout.propTypes = {
+  title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class Index extends React.Component {
+  render() {
+    const { data } = this.props
+    const { title } = data.site.siteMetadata
+    const { description } = data.site.siteMetadata
+    const { author } = data.site.siteMetadata
+    const lang = 'en'
 
-export default IndexPage
+    return (
+      <Layout location={this.props.location} title={title}>
+        <SEO title={title} description={description} author={author} lang={lang} />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
+}
+
+export default Index
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title,
+        description,
+        author
+      }
+    }
+  }
+`
