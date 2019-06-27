1309882f2c71c409ac5e1a36074a90e771a7ad20
Jason Rundell <jason.rundell@gmail.com>
2019-06-27 14:19:53 -0400

    Figure component added

---
+++ src/components/figure.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ImageFluid from './image-fluid'
+
+const Figure = ({ altText, caption, image }) => ({
+  render (){
+    let dataCaption = ''
+    if (caption) {
+      dataCaption = <figcaption>{caption}</figcaption>
+    }
+
+    return (
+      <figure>
+        <ImageFluid image={image} alt={altText} />
+        {dataCaption}
+      </figure>
+    )
+  }  
+})
+
+Figure.defaultProps = {
+  altText: '',
+}
+
+Figure.propTypes = {
+  altText: PropTypes.string,
+  caption: PropTypes.string,
+  image: PropTypes.object.isRequired,
+}
+
+export default Figure
