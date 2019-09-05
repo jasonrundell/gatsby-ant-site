d92e87b4ab4cd38d1260c3d6aa2422301e97b727
Jason Rundell <jason.rundell@gmail.com>
2019-09-04 23:52:44 -0400

    New pagination-list component

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import BlogPostPreviewCard from '../components/blog-post-preview-card'
+import PaginationList from '../components/pagination-list'
 
 import styles from './blog-list.module.scss'
 
 export default props => {
   const { data, pageContext } = props
   const { edges: posts } = data.allMarkdownRemark
-  const { next, prev } = pageContext // TO DO: currentPage
+  const { nextPageNumber, previousPageNumber, numPages } = pageContext
   return (
     <Layout
       title={data.site.siteMetadata.title}
@@ -40,17 +41,18 @@ export default props => {
           )
         })}
       </ul>
-      <div className={styles.page__navigation}>
-        {prev && (
-          <Link to={`/blog/${prev}`} className={styles.page__action}>
-            ← Previous page ({prev})
-          </Link>
-        )}
-        {next && (
-          <Link to={`/blog/${next}`} className={styles.page__action}>
-            Next page ({next}) →
-          </Link>
-        )}
+      <div className={styles.pagination__container}>
+        <PaginationList
+          styles={styles}
+          pagesCount={numPages}
+          path={'/blog/'}
+          previousPageNumber={previousPageNumber}
+          nextPageNumber={nextPageNumber}
+          previousBridge={'...'}
+          nextBridge={'...'}
+          previousLabel={'< Previous Page'}
+          nextLabel={'Next Page >'}
+        />
       </div>
     </Layout>
   )
