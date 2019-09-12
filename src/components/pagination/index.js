3f67d907a67fa0db76570cd10ce0d52787c66664
Jason Rundell <jason.rundell@gmail.com>
2019-09-11 23:26:45 -0400

    Additional fix for pagination

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -11,20 +11,22 @@ const Pagination = ({
 }) => (
   <nav role="navigation" aria-label="Pagination Navigation">
     <ul className={styles.pagination__navigation}>
-      <li className={styles.pagination__action}>
-        <Link to={previousUrl}>{previousLabel}</Link>
-      </li>
-      <li className={styles.pagination__action}>
-        <Link to={nextUrl}>{nextLabel}</Link>
-      </li>
+      {previousUrl && (
+        <li className={styles.pagination__action}>
+          <Link to={previousUrl}>{previousLabel}</Link>
+        </li>
+      )}
+      {nextUrl && (
+        <li className={styles.pagination__action}>
+          <Link to={nextUrl}>{nextLabel}</Link>
+        </li>
+      )}
     </ul>
   </nav>
 )
 
 Pagination.defaultProps = {
   styles: { pagination__navigation: '', pagination__action: '' },
-  previousUrl: '',
-  nextUrl: '',
   previousLabel: 'Previous',
   nextLabel: 'Next',
 }
