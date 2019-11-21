26d15998ade17e10a4c479850bb52ba451438e1c
Jason Rundell <jason.rundell@gmail.com>
2019-11-20 23:28:26 -0500

    Removing unused code

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,4 @@
 import { configure } from '@storybook/react'
-import { addParameters } from '@storybook/react'
-import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks'
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.js$/), module)
-
-addParameters({
-  docs: {
-    container: DocsContainer,
-    page: DocsPage,
-  },
-})
