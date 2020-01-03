fd0a7edfadfdf525a0827f65958e1e822d083a14
Jason Rundell <jason.rundell@gmail.com>
2020-01-03 00:52:22 -0500

    New Ant implementations and removing unused custom components

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,38 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'gatsby'
+import { Layout, Menu } from 'antd'
 
 import SkipToMain from '../skip-to-main'
-import Header from '../header'
+// import Header from '../header'
 import Breadcrumb from '../breadcrumb'
 
 import styles from './layout.module.scss'
 
-const Layout = ({ title, crumbs, children }) => {
-  let crumbNode = crumbs ? <Breadcrumb crumbs={crumbs} /> : null
+import 'antd/dist/antd.css'
+
+const { Header, Content, Footer } = Layout
+
+const _Layout = ({ title, crumbs, children }) => {
   return (
     <>
       <SkipToMain />
-      <Header siteTitle={title} />
-      <div className={styles.root}>
-        {crumbNode}
-        <main id="main">{children}</main>
-        <footer className={styles.footer}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org" rel="noopener noreferrer">
-            Gatsby
-          </a>
-          .{` `}
-          Calin says hello.
-        </footer>
-      </div>
+      <Header>
+        <Link to="/" className={styles.siteName}>
+          {title}
+        </Link>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/blog/">Blog</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/about/">About</Link>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Link to="/contact/">Contact</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content id="main" className={styles.root}>
+        {crumbs && <Breadcrumb crumbs={crumbs} />}
+        {children}
+      </Content>
+      <Footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org" rel="noopener noreferrer">
+          Gatsby
+        </a>
+        .{` `}
+        Calin says hello.
+      </Footer>
     </>
   )
 }
 
-Layout.propTypes = {
+_Layout.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default _Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
+import { Typography } from 'antd'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import Section from '../components/section'
-import Button from '../components/button'
-import Icon from '../components/icons'
+
+const { Title, Paragraph } = Typography
 
 export default ({ data }) => (
   <Layout title={data.site.siteMetadata.title}>
@@ -15,40 +15,11 @@ export default ({ data }) => (
       author={data.site.siteMetadata.author}
       lang={data.site.siteMetadata.lang}
     />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <Section isLarge={true}>
-      <Button>Default</Button>
-      <Button isBrand={true}>Brand</Button>
-      <Button isLight={true}>Light</Button>
-      <Button isDark={true}>Dark</Button>
-      <div style={{ backgroundColor: '#000000', padding: '16px' }}>
-        <Button isBrandInverse={true}>Brand Inverse</Button>
-        <Button isLightInverse={true}>Light Inverse</Button>
-        <Button isDarkInverse={true}>Dark Inverse</Button>
-      </div>
-    </Section>
-    <Section isConstrained={true}>
-      <Icon icon="checkmark" />
-      <Icon icon="checkmark" isBrand={true} />
-      <Icon icon="checkmark" isLight={true} />
-      <Icon icon="checkmark" isDark={true} />
-    </Section>
-    <Section>
-      <Icon icon="dot" />
-      <Icon icon="dot" isBrand={true} />
-      <Icon icon="dot" isLight={true} />
-      <Icon icon="dot" isDark={true} />
-    </Section>
-    <Section>
-      <Icon icon="tooltip" />
-      <Icon icon="tooltip" isBrand={true} />
-      <Icon icon="tooltip" isLight={true} />
-      <Icon icon="tooltip" isDark={true} />
-    </Section>
-    <Section>
+    <Title>Hey world</Title>
+    <Paragraph>Welcome to your new Gatsby site.</Paragraph>
+    <Paragraph>
       <Link to="/blog/">Go to the Blog</Link>
-    </Section>
+    </Paragraph>
   </Layout>
 )
 
