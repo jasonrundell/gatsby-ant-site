677165c94d5ba1dac71f77e55bbc9f11bbb5db90
Jason Rundell <jason.rundell@gmail.com>
2020-01-20 22:39:05 -0500

    ImageFluid case fix

--- src/components/__tests__/ImageFluid.test.js
+++ src/components/__tests__/ImageFluid.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 
-import ImageFluid from '../image-fluid'
+import ImageFluid from '../ImageFluid'
 
 describe('ImageFluid', () => {
   it('renders correctly', () => {
