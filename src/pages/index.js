dcc30d3186cf0018c58d4a6943bdd20677ad3801
Jason Rundell <jason.rundell@gmail.com>
2020-02-13 00:54:08 -0500

    Removing unused method

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import { Typography, Row } from 'antd'
 
 import Layout from '../components/Layout'
